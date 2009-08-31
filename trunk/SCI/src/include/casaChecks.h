2f08cbfc52f73ee689b1acd7c80bf0297b3ec0a5
bhatnagar.sanjay <bhatnagar.sanjay@6c983b56-0e34-0410-bb18-b3d48ac8bcd4>
2009-08-31 17:39:30 +0000

    Made directories for CAAPS related lib and include source files. Moved casaChecks.cc to lib and casaChecks.h to include

---
+++ trunk/SCI/src/include/casaChecks.h
@@ -0,0 +1,12 @@
+#include <casa/aips.h>
+#include <casa/BasicSL/String.h>
+#include <casa/Exceptions/Error.h>
+#include <casa/sstream.h>
+#include <strstream>
+
+namespace casa
+{
+  Bool checkCASAEnv(String pathVar="CASAPATH");
+};
+
+
---
+++ trunk/SCI/src/lib/casaChecks.cc
@@ -0,0 +1,23 @@
+#include <casa/aips.h>
+#include <casa/BasicSL/String.h>
+#include <casa/Exceptions/Error.h>
+#include <casa/sstream.h>
+#include <strstream>
+
+namespace casa 
+{
+  Bool checkCASAEnv(String pathVar="CASAPATH")
+  {
+    if (!getenv(pathVar.c_str()))
+      {
+	ostringstream msg;
+	msg << "Environment variable " << pathVar << " not found. " << endl
+	    << "Perhaps you forgot to source casainit.sh/.csh?";
+	
+	throw(AipsError(msg.str()));
+      }
+    return True;
+  }
+};
+
+
