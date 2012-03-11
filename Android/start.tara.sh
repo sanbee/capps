71ae21c4abbde2362a5822dcb44b57d576616196
bhatnagar.sanjay <bhatnagar.sanjay@6c983b56-0e34-0410-bb18-b3d48ac8bcd4>
2012-03-11 17:26:58 +0000

    install.readme: Information on how to install all the components required for android development on Linux.
    start.*.sh: Scripts to access all software components required for android development on the named macine.

---
+++ Android/start.tara.sh
@@ -0,0 +1,3 @@
+setenv PATH /home/tara/sanjay/bin/apache-ant-1.8.2/bin:/home/tara/sanjay/bin/android-sdk-linux_x86/tools/:/home/tara/sanjay/bin/android-sdk-linux_x86/platform-tools:$PATH
+setenv LD_LIBRARY_PATH /home/tara/sanjay/lib/zlib-1.2.5:$LD_LIBRARY_PATH
+setenv JAVA_HOME /home/tara/sanjay/bin/jdk1.5.0_22
