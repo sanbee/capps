9e2e90210da23bc470c88d8f24e99e3275128fac
bhatnagar.sanjay <bhatnagar.sanjay@6c983b56-0e34-0410-bb18-b3d48ac8bcd4>
2013-08-23 22:55:24 +0000

    Calling the blank CUDA kernel to check that plumbing is finally all in place (PHEEEEEEEEEW!).

--- trunk/SCI/src/GPU/cDataToGridImpl.h
+++ trunk/SCI/src/GPU/cDataToGridImpl.h
@@ -19,7 +19,15 @@ namespace casa { //# NAMESPACE CASA - BEGIN
 				  const Double *dphase_ptr, Int XThGrid, Int YThGrid);
 
   template <class T>
-  __global__ void cuDataToGridImpl_p(T* gridStore, Int* gridShape, VBStore* vbs,
+  void cuDataToGridImpl_p(T* gridStore, Int* gridShape, VBStore* vbs,
+				     Matrix<Double>* sumwt, 
+				     const Bool dopsf ,
+				     const Int* polMap_ptr, const Int *chanMap_ptr,
+				     const Double *uvwScale_ptr, const Double *offset_ptr,
+				     const Double *dphase_ptr, Int XThGrid=0, Int YThGrid=0
+				     );
+  template <class T>
+  __global__ void kernel_cuDataToGridImpl_p(T* gridStore, Int* gridShape, VBStore* vbs,
 				     Matrix<Double>* sumwt, 
 				     const Bool dopsf ,
 				     const Int* polMap_ptr, const Int *chanMap_ptr,
