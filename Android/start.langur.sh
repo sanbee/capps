1229c0de8c4b6d02a5931a4e37831d4d8abc40f7
bhatnagar.sanjay <bhatnagar.sanjay@6c983b56-0e34-0410-bb18-b3d48ac8bcd4>
2014-10-23 20:51:36 +0000

    Minor changes after move to RHEL6

--- Android/start.langur.sh
+++ Android/start.langur.sh
@@ -1,3 +1,4 @@
-setenv PATH /export/home/langur/sbhatnag/bin/apache-ant-1.8.2/bin:/export/home/langur/sbhatnag/bin/android-sdk-linux_x86/tools:/export/home/langur/sbhatnag/bin/android-sdk-linux_x86/platform-tools:$PATH
+# setenv PATH /export/home/langur/sbhatnag/bin/apache-ant-1.8.2/bin:/export/home/langur/sbhatnag/bin/android-sdk-linux_x86/tools:/export/home/langur/sbhatnag/bin/android-sdk-linux_x86/platform-tools:/export/home/langur/sbhatnag/bin/android-sdk-linux_x86/adt-bundle-linux-x86_64-20140702/sdk:$PATH
+setenv PATH /export/home/langur/sbhatnag/bin/apache-ant-1.8.2/bin:/export/home/langur/sbhatnag/bin/android-sdk-linux_x86/adt-bundle-linux-x86_64-20140702/sdk/tools:/export/home/langur/sbhatnag/bin/android-sdk-linux_x86/adt-bundle-linux-x86_64-20140702/sdk/platform-tools:$PATH
 #setenv LD_LIBRARY_PATH /home/tara/sanjay/lib/zlib-1.2.5:$LD_LIBRARY_PATH
-setenv JAVA_HOME  ~/Downloads/jdk1.5.0_22/
+#setenv JAVA_HOME  ~/Downloads/jdk1.5.0_22/
