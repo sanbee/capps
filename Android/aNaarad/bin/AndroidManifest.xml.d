0b5b39abb3c1c08eeb439733771139a27bf59c44
bhatnagar.sanjay <bhatnagar.sanjay@6c983b56-0e34-0410-bb18-b3d48ac8bcd4>
2014-10-23 20:48:37 +0000

    Dark ages version (i.e. Ver. 0.1)

---
+++ Android/aNaarad/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/export/home/langur/sbhatnag/Projects/Android/aNaarad/bin/AndroidManifest.xml : \
+/export/home/langur/sbhatnag/Projects/Android/aNaarad/AndroidManifest.xml \
---
+++ Android/aNaarad/bin/NaaradClientActivity-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/export/home/langur/sbhatnag/Projects/Android/aNaarad/bin/NaaradClientActivity-debug-unaligned.apk : \
+/export/home/langur/sbhatnag/Projects/Android/aNaarad/bin/NaaradClientActivity.ap_ \
+/export/home/langur/sbhatnag/Projects/Android/aNaarad/bin/classes.dex \
+/export/home/langur/sbhatnag/Projects/Android/aNaarad/libs/android-support-v4.jar \
